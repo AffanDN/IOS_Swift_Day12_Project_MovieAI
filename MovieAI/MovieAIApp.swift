6a2477b3b1b60e588dad2c50b8a5e0fc93e1f7ed
AffanDN <affan03nusli@gmail.com>
2024-05-02 09:46:25 +0700

    Initial Commit

---
+++ MovieAI/MovieAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAIApp.swift
+//  MovieAI
+//
+//  Created by Macbook Pro on 02/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
